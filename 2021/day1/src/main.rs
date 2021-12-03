ef456bc0243a6cc1110a8b9c3834a3f0ce503dde
Jan Soendermann <jan.soendermann+git@gmail.com>
2021-12-03 09:44:49 +0000

    Day 1

---
+++ 2021/day1/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+fn main() {
+    let res = fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|l| l.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>()
+        .windows(2)
+        .filter(|w| w[0] < w[1])
+        .count();
+        
+    println!("Part 1: {}", res);
+
+    let res = fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|l| l.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>()
+        .windows(3)
+        .map(|w| w.iter().sum())
+        .collect::<Vec<usize>>()
+        .windows(2)
+        .filter(|w| w[0] < w[1])
+        .count();
+    
+        println!("Part 2: {}", res);
+}
